bdfd3c39cea461c2375ae4f288ff3d38823337e4
nao <nao@e33849b5-4ee8-4e00-b6aa-67194e0aece3>
2014-05-01 03:11:09 +0000

    アイコンハンドリング

--- decode.cpp
+++ decode.cpp
@@ -3,28 +3,28 @@
 #include <vcl.h>
 #pragma hdrstop
 //---------------------------------------------------------------------------
-USEFORM("Unit1.cpp", Form1);
-USEFORM("Unit2.cpp", Form2);
-USEFORM("Unit3.cpp", Form3);
 USEFORM("Unit4.cpp", Form4);
-USEFORM("Unit5.cpp", Form5);
-USEFORM("Unit6.cpp", ParmDialog);
+USEFORM("Unit3.cpp", Form3);
+USEFORM("Unit2.cpp", Form2);
 USEFORM("Unit7.cpp", Form7);
-//---------------------------------------------------------------------------
-WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+USEFORM("Unit6.cpp", ParmDialog);
+USEFORM("Unit5.cpp", Form5);
+USEFORM("Unit1.cpp", Form1);
+W//---------------------------------------------------------------------------
+INAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
         try
         {
                  Application->Initialize();
                  Application->Title = "basic analysis";
-                 Application->CreateForm(__classid(TForm1), &Form1);
+                 ApApplication->CreateForm(__classid(TForm1), &Form1);
 		Application->CreateForm(__classid(TForm2), &Form2);
 		Application->CreateForm(__classid(TForm3), &Form3);
 		Application->CreateForm(__classid(TForm4), &Form4);
 		Application->CreateForm(__classid(TForm5), &Form5);
 		Application->CreateForm(__classid(TParmDialog), &ParmDialog);
 		Application->CreateForm(__classid(TForm7), &Form7);
-		Application->Run();
+		plication->Run();
         }
         catch (Exception &exception)
         {
